6a9666658ba655a482d54d5850caf97c9caf81a6
SravKarra <marutiskarra89@gmail.com>
2021-04-02 14:52:18 -0400

    created functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+/* function multiply (num1,num2){
+    let result = num1*num2;
+    return result;
+}
+let result = multiply(2,5);
+console.log(result); */
+
+function myFunc(theObject){
+    theObject.brand='Toyota';
+}
+var mycar={
+    brand:'Toyota';
+    model:'RAV4';
+    year:'2012'
+};
+console.log(mycar.brand)
